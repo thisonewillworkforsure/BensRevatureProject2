94f0f32b5c7adc8121d23d702f38a410548696a0
thisonewillworkforsure <ben6228@yahoo.com>
2022-09-15 15:51:57 -0400

    using annotations, added exception package

---
+++ src/main/java/com/project2/springbootwebjdbcdatalombok/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.project2.springbootwebjdbcdatalombok.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.project2.springbootwebjdbcdatalombok.entity.UsersEntity;
+
+//Repository annotation is used to indicate that the class provides
+//the mechanism for CRUD operations on objects
+@Repository
+public interface UserDao extends JpaRepository<UsersEntity, Integer> {
+
+	// if you want to work with cutomized queries(other than the crud queries),  write finder methods
+		// SELECT * from book_details WHERE book_genre='Fiction';
+}
