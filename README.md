b9643f7de2b5b0d367e7e1cacb57b4b3989802cb
thisonewillworkforsure <ben6228@yahoo.com>
2022-09-15 15:25:46 -0400

    first commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# BensRevatureProject2
---
+++ src/main/java/com/project2/springbootwebjdbcdatalombok/SpringBootWebJdbcDataLombokApplication.java
@@ -0,0 +1,13 @@
+package com.project2.springbootwebjdbcdatalombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebJdbcDataLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebJdbcDataLombokApplication.class, args);
+	}
+
+}
