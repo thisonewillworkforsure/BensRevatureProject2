4ce789a7ed7f6975f750e835f86372d853da67a7
thisonewillworkforsure <ben6228@yahoo.com>
2022-09-26 17:48:12 -0400

    finished Purchase history testing!

--- src/test/java/com/project2/springbootwebjdbcdatalombok/PurchaseServiceImpTest.java
+++ src/test/java/com/project2/springbootwebjdbcdatalombok/PurchaseServiceImpTest.java
@@ -1,6 +1,8 @@
 package com.project2.springbootwebjdbcdatalombok;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,13 +12,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.BeanUtils;
 
 import com.project2.springbootwebjdbcdatalombok.dao.ProfileDao;
 import com.project2.springbootwebjdbcdatalombok.dao.PurchaseDao;
 import com.project2.springbootwebjdbcdatalombok.entity.ProfileEntity;
 import com.project2.springbootwebjdbcdatalombok.entity.PurchaseEntity;
 import com.project2.springbootwebjdbcdatalombok.entity.PurchaseHistoryEntity;
+import com.project2.springbootwebjdbcdatalombok.entity.ShoppingCartItemEntity;
+import com.project2.springbootwebjdbcdatalombok.exception.ApplicationException;
 import com.project2.springbootwebjdbcdatalombok.pojo.ProfilePojo;
 import com.project2.springbootwebjdbcdatalombok.pojo.PurchasePojo;
 import com.project2.springbootwebjdbcdatalombok.pojo.ShoppingCartItemPojo;
@@ -80,7 +86,66 @@ public class PurchaseServiceImpTest {
 	
 	@Test
 	public void testUpdatePurchase() {
-		assertEquals(1, 1);
+		purchasePojo.setTotalCost(3000);
+		
+		try {
+			Mockito.when(purchaseDao.save(any())).thenReturn(purchaseEntity);
+			purchasePojo = purchaseServiceImp.updatePurchase(otherPojo);
+		} catch (ApplicationException e) {
+			// TODO Auto-generated catch block
+			assertEquals(0, 1);
+		}
+		assertEquals(purchasePojo.getTotalCost(), otherPojo.getTotalCost());
+	}
+	
+	@Test
+	public void testCreatePurchase() {
+	purchasePojo.setTotalCost(3000);
+		
+		try {
+			Mockito.when(purchaseDao.saveAndFlush(any())).thenReturn(purchaseEntity);
+			purchasePojo = purchaseServiceImp.createPurchase(otherPojo);
+		} catch (ApplicationException e) {
+			// TODO Auto-generated catch block
+			assertEquals(0, 1);
+		}
+		assertEquals(purchasePojo.getTotalCost(), otherPojo.getTotalCost());
+	}
+	
+	@Test
+	public void testGetAllPurchase() {
+		
+		
+		BeanUtils.copyProperties(purchasePojo, purchaseEntity);
+		purchaseEntity.setShoppingCartItemEntities(new ArrayList<ShoppingCartItemEntity>());
+		purchaseEntities.add(purchaseEntity);
+		
+		try {
+			Mockito.when(purchaseDao.findAll()).thenReturn(purchaseEntities);
+			purchasePojos = purchaseServiceImp.getAllPurchase();
+		} catch (ApplicationException e) {
+			// TODO Auto-generated catch block
+			assertEquals(0, 1);
+		}
+		assertEquals(purchasePojos.size(), purchaseEntities.size());
+		
+	}
+	
+	@Test
+	public void testGetAllPurchaseForOne() {
+		BeanUtils.copyProperties(purchasePojo, purchaseEntity);
+		purchaseEntity.setShoppingCartItemEntities(new ArrayList<ShoppingCartItemEntity>());
+		purchaseEntities.add(purchaseEntity);
+		
+		try {
+			Mockito.when(purchaseDao.findByUserID(anyInt())).thenReturn(purchaseEntities);
+			purchasePojos = purchaseServiceImp.getAllPurchaseForOne(purchasePojo.getUserID());
+		} catch (ApplicationException e) {
+			// TODO Auto-generated catch block
+			assertEquals(0, 1);
+		}
+		assertEquals(purchasePojos.size(), purchaseEntities.size());
+		
 	}
 	
 }
